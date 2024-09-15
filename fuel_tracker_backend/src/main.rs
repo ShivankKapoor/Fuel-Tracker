25fb611e7f349f5513705ee0128f533aeeb17f55
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-09-15 02:26:30 -0500

    Add all models

--- fuel_tracker_backend/src/main.rs
+++ fuel_tracker_backend/src/main.rs
@@ -3,6 +3,7 @@ use axum::{routing::{get, post}, Router, Json, extract::State};
 use sqlx::PgPool; // For managing a connection pool to PostgreSQL
 use dotenv::dotenv; // For loading environment variables from a .env file
 use std::env; // For accessing environment variables
+use chrono::NaiveDate;
 
 mod models;
 
--- fuel_tracker_backend/src/models.rs
+++ fuel_tracker_backend/src/models.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
+use chrono::NaiveDate;
+
 
 #[derive(Serialize, Deserialize)]
 pub struct Users {
     pub id: i32,
-    pub username: String, // Handle this field as String if necessary
+    pub username: String,
     pub password_hash: String,
 }
 
@@ -14,4 +16,23 @@ pub struct Cars {
     pub make: String,
     pub model: String,
     pub year: i32,
+}
+
+#[derive(Deserialize)]
+pub struct CarCheckIns {
+    pub checkin_id: i32,
+    pub car_id: i32,
+    pub checkin_date: NaiveDate,
+    pub miles: i32,
+    pub tank_left: i32,
+}
+
+#[derive(Deserialize)]
+pub struct FillUps {
+    pub fillup_id: i32,
+    pub car_id: i32,
+    pub fillup_date: NaiveDate,
+    pub gallons_filled: f64,
+    pub price_per_gallon: f64,
+    pub new_range: i32,
 }
