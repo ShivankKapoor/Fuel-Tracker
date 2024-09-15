5f896c099a103491185dd2b11ce8c2481f32fecd
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-09-15 03:38:47 -0500

    new message if DB is down

--- fuel_tracker_backend/src/db.rs
+++ fuel_tracker_backend/src/db.rs
@@ -1,14 +1,9 @@
 use sqlx::PgPool;
-use crate::models::{Users, Cars, CarCheckIns, FillUps};
-use sqlx::Result; 
+use sqlx::Result;
 
 pub async fn is_up(pool: &PgPool) -> Result<String> {
-    let result = sqlx::query("SELECT 1")
-        .execute(pool)
-        .await;
-
-    match result {
+    match sqlx::query("SELECT 1").execute(pool).await {
         Ok(_) => Ok("A.P.I is up!".to_string()),
-        Err(err) => Err(err),
+        Err(_) => Ok("DB is Down but A.P.I is up".to_string()),
     }
 }
